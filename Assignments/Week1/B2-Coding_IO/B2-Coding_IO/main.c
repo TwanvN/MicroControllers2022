84b9081eacbe3ddf68a312e1e3d8f373ad6786c1
Jesse <jm.krijgsman@student.avans.nl>
2022-02-03 10:01:00 +0100

    Documented B2

--- Assignments/Week1/B2-Coding_IO/B2-Coding_IO/main.c
+++ Assignments/Week1/B2-Coding_IO/B2-Coding_IO/main.c
@@ -1,10 +1,14 @@
 /*
  * main.c
  *
- * Created: 2/2/2022 11:28:58 AM
- *  Author: Jesse
+ * Project created for the B1 assignment of week 1 micro controllers.
+ * Code will make the PORTD pin 6 and 7 light up and flicker
+ *
+ * Created: 2/2/2022 10:33:08 AM
+ *  Author: Jesse Krijgsman & Twan van Noorloos
  */ 
 
+// Defining the CPU speed to make the _delay_ms function work
 #define F_CPU 8e6
 
 
@@ -12,8 +16,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+/*
+* Wait method was created by Etiënne Goossens and taken from the example projects given.
+* Given the amount of milliseconds to wait the method waits for that long.ss
+*/
 void wait( int ms )
 {
+	// Make the processor delay for 1 ms looped for the given ms
 	for (int i=0; i<ms; i++) {
 		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
 	}
@@ -22,14 +31,15 @@ void wait( int ms )
 
 int main(void)
 {
-	DDRD = 0b11000000;
+	DDRD = 0xC0;	// Turning pin 6 and 7 to output, rest to input 0b1100-0000;
 	
+	// Main loop
     while(1)
     {
-        PORTD = 0b10000000;
-		wait(500);
-		PORTD = 0b01000000;
-		wait(500);
+        PORTD = 0x80;	// Turning pin 7 on and 6 off 0b1000-0000
+		wait(500);		// Delay for 500 ms
+		PORTD = 0x40;	// Turning pin 6 on and 7 off 0b0100-0000
+		wait(500);		// Delay for 500 ms
     }
 	
 	return 1;
