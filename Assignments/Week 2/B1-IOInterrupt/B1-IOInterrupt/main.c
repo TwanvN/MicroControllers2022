0c0022f701daed3132aab65ddd80c6cb4e801b68
Twan van Noorloos <twan.v.n@outlook.com>
2022-02-09 11:08:55 +0100

    Added excersice B1 and B2 of week 2

---
+++ Assignments/Week 2/B1-IOInterrupt/B1-IOInterrupt/main.c
@@ -0,0 +1,87 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** ioisr.c
+**
+** Beschrijving:	ISR on PORTD demonstrattion  
+** Target:			AVR mcu
+** Build:			avr-gcc -std=c99 -Wall -O3 -mmcu=atmega128 -D F_CPU=8000000UL -c ioisr.c
+**					avr-gcc -g -mmcu=atmega128 -o ioisr.elf ioisr.o
+**					avr-objcopy -O ihex ioisr.elf ioisr.hex 
+**					or type 'make'
+** Author: 			dkroeske@gmail.com
+**
+** Code has been copied from Etiënne Goossens from GitHub
+** -------------------------------------------------------------------------*/
+
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+/******************************************************************
+short:			Busy wait number of millisecs
+inputs:			int ms (Number of millisecs to busy wait)
+outputs:	
+notes:			Busy wait, not very accurate. Make sure (external)
+				clock value is set. This is used by _delay_ms inside
+				util/delay.h
+Version :    	DMK, Initial code
+*******************************************************************/
+void wait( int ms ) {
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+/*****************************************************************
+short:			ISR INT0
+inputs:
+outputs:
+notes:			Set PORTD.5
+Version :    	DMK, Initial code
+*******************************************************************/
+ISR( INT0_vect ) {
+    PORTD |= (1<<5);		
+}
+
+/******************************************************************
+short:			ISR INT1
+inputs:
+outputs:
+notes:			Clear PORTD.5
+Version :    	DMK, Initial code
+*******************************************************************/
+ISR( INT1_vect ) {
+    PORTD &= ~(1<<5);		
+}
+
+/******************************************************************
+short:			main() loop, entry point of executable
+inputs:
+outputs:
+notes:			Slow background task after init ISR
+Version :    	DMK, Initial code
+*******************************************************************/
+int main( void ) {
+	// Init I/O
+	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input	
+
+	// Init Interrupt hardware
+	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
+	EIMSK |= 0x03;			// Enable INT1 & INT0
+	
+	// Enable global interrupt system
+	//SREG = 0x80;			// Of direct via SREG of via wrapper
+	sei();				
+
+	while (1) {
+		PORTD ^= (1<<7);	// Toggle PORTD.7
+		wait( 500 );								
+	}
+
+	return 1;
+}
---
+++ Assignments/Week 2/B2-Looplicht/B2-Looplicht/main.c
@@ -0,0 +1,56 @@
+/*
+ * main.c
+ *
+ * Created: 2/9/2022 10:33:04 AM
+ *  Author: Twan van Noorloos & Jesse Krijgsman
+ */ 
+
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+/*
+ * Interrupt method which changes the light on PORTC
+ * Does this by bithhifting PORTC one to the left and checks
+ * if the value is at the end and sets PORTC to the first LED
+ */
+ISR( INT1_vect ) {
+	PORTC <<= 1;
+	
+	if (PORTC == 0b00000000)
+		PORTC = 0x01;
+}
+
+/*
+ * Interrupt method which changes the light on PORTC
+ * Does this by bitshifting PORTC one to the right and checks
+ * if the value is at the start and sets PORTC to the last LED
+ */
+ISR( INT2_vect ) {
+	PORTC >>= 1;
+	
+	if (PORTC == 0b00000000)
+		PORTC = 0b10000000;
+}
+
+int main( void ) {
+	// Init I/O
+	DDRD = 0b11111001;
+	DDRC = 0xFF;
+
+	// Init Interrupt hardware
+	EICRA |= 0x3C;			// INT2 rising edge, INT1 rising edge
+	EIMSK |= 0x06;			// Enable INT2 & INT1
+	
+	// Enable global interrupt system
+	sei();
+	
+	PORTC = 0x01;
+
+	while (1) {
+		
+	}
+
+	return 1;
+}
