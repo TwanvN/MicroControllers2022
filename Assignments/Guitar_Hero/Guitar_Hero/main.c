992434d958ce75ea1fb5dc592cda6e351100ab02
Jesse <jm.krijgsman@student.avans.nl>
2022-03-10 15:31:26 +0100

    Created eindopdracht project

---
+++ Assignments/Guitar_Hero/Guitar_Hero/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/10/2022 3:30:28 PM
+ *  Author: Jesse
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
