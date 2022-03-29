c6abdfafcf4f1c6ce0915648404c104a7b03ed0f
Jesse <jm.krijgsman@student.avans.nl>
2022-03-29 11:26:44 +0200

    Created ultrasone module to use

--- Assignments/Guitar_Hero/Guitar_Hero/main.c
+++ Assignments/Guitar_Hero/Guitar_Hero/main.c
@@ -4,7 +4,6 @@
  * Created: 3/10/2022 3:30:28 PM
  *  Author: Jesse
  */ 
-
 #define F_CPU 8e6
 
 #include <avr/io.h>
@@ -16,41 +15,7 @@
 #include <xc.h>
 
 #include "LCD_Module.h"
-
-#define TRIGGER_PIN 0
-#define ECHO_PIN 7
-
-#define TICK_CM 58.0
-
-int currentDistance = 0;
-
-char ticksOnTrigger = 0;
-
-ISR ( INT7_vect )
-{	
-	PORTD = PINE;
-	
-	if( PINE & (1 << ECHO_PIN) )
-		ticksOnTrigger = TCNT2;
-	
-	else
-	{
-		unsigned char diff = TCNT2 - ticksOnTrigger;	// Getting the difference
-		
-		currentDistance = ( diff * ( 32.0 / TICK_CM ) );	
-				
-		
-		ticksOnTrigger = 0; // resetting ticksOnTrigger
-	}
-	
-}
-
-ISR ( TIMER2_OVF_vect )
-{	
-	TCNT2 = 0; // Resetting timer
-}
-
-void initTimer();
+#include "UTS_Ultrasone.h"
 
 int main(void)
 {	
@@ -58,45 +23,18 @@ int main(void)
 	
 	// Init I/O
 	DDRD = 0xFF;			// Port D to output
-
-	DDRE = 0b00001111;		// PORTE(7:4) input, PORTE(3:0) output
-
-	// Init Interrupt hardware
-	EICRB |= 0x40;			// ISC7 Rising edge
-	EIMSK |= 0x80;			// Enable INT7
-		
-	initTimer();
-	
-	// Initing the LCD module
-	LCD_init();
-	
-	_delay_ms(1500);
+			
+	UTS_Init();
 					
     while (1) {		
-		PORTE |= (1 << TRIGGER_PIN);
 		
-		_delay_us(10);
+		UTS_Trigger();
 		
-		PORTE &= ~(1 << TRIGGER_PIN);
-		
-		char string[13];
-		
-		// Writing distance to LCD
-		sprintf(string, "%d cm   ", currentDistance);
-		LCD_set_cursor(0);
-		LCD_display_text(string);
+		PORTD = currentDistance;
 		
 		
-	    _delay_ms( 1000 );
+	    _delay_ms( 250 );
     }
 	
 	return 1;
-}
-
-void initTimer()
-{
-	TCNT2 = 0;
-	TIMSK |= 0b01000000;
-	TCCR2 = 0b00001100;
-	sei();
 }
