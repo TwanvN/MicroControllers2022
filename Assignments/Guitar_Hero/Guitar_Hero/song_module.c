4118821eb685697743698e099e86a63f8007a3b6
Twan van Noorloos <twan.v.n@outlook.com>
2022-03-31 13:05:13 +0200

    Added comments to song_module.c

--- Assignments/Guitar_Hero/Guitar_Hero/song_module.c
+++ Assignments/Guitar_Hero/Guitar_Hero/song_module.c
@@ -11,27 +11,46 @@
 #include "song_module.h"
 #include "UTS_Ultrasone.h"
 
+/************************************************************************/
+/* Struct which contains 2 variables for the light to light up and the  */
+/* corresponding pwm signal to send to the buzzer						*/
+/************************************************************************/
 typedef struct SongPattern {
 	int lightID;
 	int pwmSignal;
 } SONG_PATTERN;
 
+/************************************************************************/
+/* Array of SongPattern structs to create a pattern for the lights      */
+/* to light up.															*/
+/************************************************************************/
 SONG_PATTERN firstSong[10] = {
 	{1, 800},
 	{2, 1000},
 	{3, 800},
-	{4, 600},
+	{6, 600},
 	{2, 400},
 	{3, 200},
 	{2, 100}	
 };
 
+/************************************************************************/
+/* Variable which keeps track off the current light                     */
+/************************************************************************/
 int currentLight = 0;
 
+/************************************************************************/
+/* Method which updates the current light                               */
+/************************************************************************/
 void updateLight() {
 	currentLight++;
 }
 
+/************************************************************************/
+/* Method which checks if a button is pressed corresponding to the      */
+/* light and plays the corresponding frequency. If the button is		*/
+/* incorrect the buzzer won't play any frequency.						*/
+/************************************************************************/
 void playFirstSong() {
 		
 		if (PINB == 0b00000001 << firstSong[currentLight].lightID && currentDistance < 20)
